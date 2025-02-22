c12797790bf522c5d00eca5330c9af46fd0871a0
root <mohammad.yasser.a.98@gmail.com>
2025-02-22 23:52:02 +0300

    first commit

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,14 @@
+const BurgerStack = (props)=>{
+    return (
+       
+         <ul>
+            {props.stack.map((ingredient) => (
+                <li style={{backgroundColor: ingredient.color}} onClick={()=>props.handleRemoveFromStack(ingredient)}>
+                    {ingredient.name}
+                </li>
+            ))}
+        </ul>
+        
+    )
+}
+export default BurgerStack
---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,13 @@
+const IngredientList = (props) => {
+    return (
+        <ul>
+            {props.availableIngredients.map((ingredient) => (
+                <li style={{backgroundColor: ingredient.color}} onClick={() => props.handleAddToStack(ingredient)}>
+                    {ingredient.name}
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default IngredientList
